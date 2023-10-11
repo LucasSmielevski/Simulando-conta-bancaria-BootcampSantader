5c3067b30441fea52a9aadb9b4b1f0095d76c81b
Lucas Smielevski <110873756+LucasSmielevski@users.noreply.github.com>
2023-10-11 16:14:18 -0300

    Meu projeto Conta Banco

---
+++ ContaBanco/src/contabanco/ContaTerminal.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contabanco;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lucas
+ */
+public class ContaTerminal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int Numero;
+        String Agencia;
+        String Nome_cliente;
+        double Saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor , digite o número da Conta:");
+        Numero = sc.nextInt();
+        System.out.println("Por favor , digite o número da Agência:");
+        Agencia = sc.next();
+        System.out.println("Por favor , digite o seu Nome:");
+        Nome_cliente = sc.nextLine();
+        
+        sc.nextLine();
+        
+        System.out.println("Por favor , digite o Saldo da Conta:");
+        Saldo = sc.nextDouble();
+        
+        System.out.println("Olá " + Nome_cliente + " obrigado por criar uma conta em nosso banco,"
+                + " sua agência é " + Agencia + ", conta " + Numero + " \ne seu saldo " + Saldo + ", já está "
+        + "disponível para saque" );
+    }
+
+}
